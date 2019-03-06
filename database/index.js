e283722f852b27bba1faad88119c9a89ca764acd
Austin <acreed1998@gmail.com>
2019-03-06 16:15:10 -0600

    set up database index

---
+++ database/index.js
@@ -0,0 +1,22 @@
+// Setting up the mysql database & connection and ORM (sequelize)
+
+// 1) Database creation & Table schemas
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  // host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+connection.connect((err) => {
+  if (!err) {
+    console.log('Houston, we have a db connection');
+  } else {
+    console.error('There was a problem connecting to the db. Error: ', err);
+  }
+});
+
+module.exports.connection = connection;
